92afb866735337c5d800fde613968953ec2af3f8
Becky Bell <rlbell@alum.bu.edu>
2014-03-14 10:55:12 -0400

    created new rails application for hospital survey

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HospitalSurvey::Application.config.secret_key_base = '3e01f751c70c31b8d7c6d38a271fb1b9c4be5114fdc6bc96c9b245daee39a7f8dcf88688fc1d703dd1d58d46097b6d0a8981e8043369d140f959d9a474ff5d66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HospitalSurvey::Application.config.session_store :cookie_store, key: '_hospital_survey_session'
